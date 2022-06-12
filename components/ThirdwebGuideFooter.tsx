f8d8182d420468b441c82cb6e272a8216c3a44a3
Terekhov Anatoly <anatoly.terekhov@gmail.com>
2022-06-13 00:44:52 +0300

    zeroone

--- components/ThirdwebGuideFooter.tsx
+++ components/ThirdwebGuideFooter.tsx
@@ -2,7 +2,7 @@ import styles from "../../styles/Thirdweb.module.css";
 import React from "react";
 
 export default function ThirdwebGuideFooter() {
-  const url = "https://atereta-marketplace.vercel.app/";
+  const url = "https://ybart-marketplace.vercel.app/";
   return (
     <>
       <div
